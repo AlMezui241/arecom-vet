4aef069348446f61a66dbe0782fa393bf0650d3f
AlMezui241 <148825642+AlMezui241@users.noreply.github.com>
2026-03-04 12:48:40 +0100

    Force LF line endings for build script

--- render-build.sh
+++ render-build.sh
@@ -7,10 +7,10 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Création automatique du superutilisateur si les variables sont définies
-if [[ $DJANGO_SUPERUSER_USERNAME ]]; then
-  python manage.py createsuperuser --no-input \
-    --username "admin" \
-    --email "mezui123@gmail.com" || true
-    --password "admin123" || true
-fi
+# Configuration des variables pour le superutilisateur
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_PASSWORD=admin123
+export DJANGO_SUPERUSER_EMAIL=mezui123@gmail.com
+
+# Création automatique du superutilisateur
+python manage.py createsuperuser --no-input || true
